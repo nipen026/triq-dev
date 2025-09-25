2d42830aa28cdf9f544805f5a7985d3b79b80b79
nipen026 <khantnipen@gmail.com>
2025-09-25 09:57:57 +0530

    added changes

--- controllers/customer.controller.js
+++ controllers/customer.controller.js
@@ -169,8 +169,8 @@ exports.getCustomers = async (req, res) => {
       const obj = c.toObject();
       obj.flag = getFlag(c.countryOrigin);
       obj.userImage = 'https://images.unsplash.com/vector-1741673838666-b92722040f4f?q=80&w=1480&auto=format&fit=crop&ixlib=rb-4.1.0&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D'
-      const qrPayload = JSON.stringify(c);
-      obj.qrCode = QRCode.toDataURL(qrPayload);
+      // const qrPayload = JSON.stringify(c);
+      // obj.qrCode = QRCode.toDataURL(qrPayload);
       return obj;
     });
 
