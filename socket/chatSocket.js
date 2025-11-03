1db6bc6ebfc892734d7ffcca33f5dcbcad6fb006
nipen026 <khantnipen@gmail.com>
2025-11-03 16:02:39 +0530

    added changes

--- socket/chatSocket.js
+++ socket/chatSocket.js
@@ -120,23 +120,23 @@ module.exports = (io) => {
             },
             data: {
               type: "chat_message",
-              chatRoomId: room.id,
-              screenName: "chatView",
-              Route: '/chatView',
-              chatData: {
-                contactName: socket.userId === room.organisation.id
-                  ? room.organisation.fullName
-                  : room.processor.fullName,
-                contactNumber:  socket.userId === room.organisation.id
-                  ? room.organisation.phone
-                  : room.processor.phone,
-                roomId: room.id,
-                ticketId: room.ticket ? String(room.ticket._id) : "",
-                ticketStatus: room.ticket ? room.ticket.status : "",
-                userRole:  socket.userId === room.organisation.id ? "organization" : "processor",
-                flag:  socket.userId === room.organisation.id
-                  ? getFlagWithCountryCode(room.organisation.countryCode)
-                  : getFlagWithCountryCode(room.processor.countryCode),
+              // chatRoomId: room.id,
+              // screenName: "chatView",
+              // Route: '/chatView',
+              // chatData: {
+              //   contactName: socket.userId === room.organisation.id
+              //     ? room.organisation.fullName
+              //     : room.processor.fullName,
+              //   contactNumber:  socket.userId === room.organisation.id
+              //     ? room.organisation.phone
+              //     : room.processor.phone,
+              //   roomId: room.id,
+              //   ticketId: room.ticket ? String(room.ticket._id) : "",
+              //   ticketStatus: room.ticket ? room.ticket.status : "",
+              //   userRole:  socket.userId === room.organisation.id ? "organization" : "processor",
+              //   flag:  socket.userId === room.organisation.id
+              //     ? getFlagWithCountryCode(room.organisation.countryCode)
+              //     : getFlagWithCountryCode(room.processor.countryCode),
 
               }
             },
