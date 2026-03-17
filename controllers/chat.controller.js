c652d4f2d5425c7b2b640c8a80e403b1f0d837f1
nipen-2678 <nipen@onehealtech.com>
2026-03-17 15:27:28 +0530

    add

--- controllers/chat.controller.js
+++ controllers/chat.controller.js
@@ -4,6 +4,7 @@ const { getIO } = require("../socket/socketInstance");
 const { getFlagWithCountryCode } = require("../utils/flagHelper");
 const GroupChat = require("../models/groupChat.model");
 const Role = require("../models/role.model");
+const Employee = require("../models/employee.model");
 // 🔹 POST /api/chat/rooms  → create a chat room manually
 exports.createChatRoomForTicket = async (req, res) => {
   try {
